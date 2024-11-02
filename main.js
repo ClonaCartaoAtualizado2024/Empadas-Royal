06102d6d9a88040f850b7807e2777d117de328a0
Mayarinhadograu <schneider.mayara@escola.pr.gov.br>
2024-11-01 21:01:09 -0300

    Update

---
+++ main.js
@@ -0,0 +1,8 @@
+const display = {
+    "block": document.getElementById("display"),
+    "leite": document.getElementById("display").children[0],
+    "empadinha": [document.getElementById("display").children[1],document.getElementById("display").children[2],document.getElementById("display").children[3]],
+    "sparkles": [document.getElementById("display").children[4],document.getElementById("display").children[5]]
+}
+
+console.log(display)
